08cd53c79c0b797d2c8a2c46cb37558f0adffc30
marcelo schaefer <71241113+marcelo-schaefer@users.noreply.github.com>
2023-09-26 20:59:18 -0300

    Add files via upload

--- components/perfil/perfil.js
+++ components/perfil/perfil.js
@@ -1,25 +1,28 @@
-var photoPath = "../../assets/";
+var userPhoto = document.getElementById('userPhoto');
+userPhoto.src = "../../assets/emptyPicture.jpg";
 
-validateUserPhoto();
+// validateUserPhoto();
+// var nome = document.getElementById('name')
+// nome.disabled = true;
 
-function validateUserPhoto() {
+// function validateUserPhoto() {
 
-    fetch('components\login\login.css')
-    .then(response => response.text())
-    .then(text => {
-      const array = text.split("\n");
-      console.log(array);
-    });
+//     fetch('components\login\login.css')
+//     .then(response => response.text())
+//     .then(text => {
+//       const array = text.split("\n");
+//       console.log(array);
+//     });
 
-const fs = require('../../assets/emptyPicture.jpg');
-if (!fs) {
-    photoPath =+ "emptyPicture"
- } else {
-    photoPath =+ "userPicture"
-}
+// var fs = document.require('../../assets/emptyPicture.jpg');
+// if (!fs) {
+//     photoPath =+ "emptyPicture";
+//  } else {
+//     photoPath =+ "userPicture";
+// }
 
-}
+// }
 
-function setForm(user) {
-    const password = document.getElementById("password");
-}
+// function setForm(user) {
+//     const password = document.getElementById("password");
+// }
