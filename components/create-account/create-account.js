1e7d13e18c1ab40e7b7fa9d9dc9d3382d0268e37
Lucas Nunes <69318819+Nunes03@users.noreply.github.com>
2023-09-26 20:56:22 -0300

    Add files via upload

--- components/create-account/create-account.js
+++ components/create-account/create-account.js
@@ -1,14 +1,27 @@
-const confirmPassword = document.getElementById("confirm-password");
+const createAccountButton = document.querySelector("#createAccount");
+const viewPasswordButton = document.querySelector("#viewPassword");
 
-confirmPassword.addEventListener("input", () => validatePassword());
+createAccountButton.addEventListener("click", () => validatePassword());
+viewPasswordButton.addEventListener("click", () => viewPassword());
 
 function validatePassword() {
-  const password = document.getElementById("password");
-  const messageValidation = document.getElementById("message-validation");
+  const password = document.querySelector("#password");
+  const confirmPassword = document.querySelector("#confirmPassword");
 
   if (password.value !== confirmPassword.value) {
-    messageValidation.innerHTML = "Senhas não conferem";
+    window.alert("Senhas não conferem");
+  }
+}
+
+function viewPassword() {
+  const password = document.querySelector("#password");
+  const confirmPassword = document.querySelector("#confirmPassword");
+
+  if (password.type == "password") {
+    password.type = "text";
+    confirmPassword.type = "text";
   } else {
-    messageValidation.innerHTML = "";
+    password.type = "password";
+    confirmPassword.type = "password";
   }
 }
