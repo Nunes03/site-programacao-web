7b4602117ef5e50f58612b7144bc81c79c541f1f
Nunes <lucas.nunes@senior.com.br>
2023-11-07 23:55:28 -0300

    As

--- components/home/home.js
+++ components/home/home.js
@@ -2,36 +2,65 @@ const perfilButton = document.querySelector("#perfilButton");
 
 perfilButton.addEventListener("click", () => redirectProfile());
 
-for (let i = 0; i < 10; i++) {
-
+for (let i = 0; i < 2; i++) {
+    createTrInPostTable();
 }
 
 function redirectProfile() {
     window.location.pathname = "/site-programacao-web/components/perfil/perfil.html";
 }
 
+function createTrInPostTable() {
+    const tablePosts = document.querySelector("#tablePosts");
+
+    const tr = document.createElement("tr");
+
+    const td = document.createElement("td");
+    const divPostContainer = createDivPostContainer();
+    td.appendChild(divPostContainer);
+
+    tr.appendChild(td);
+    tablePosts.appendChild(tr);
+}
+
 function createDivPostContainer() {
     const div = document.createElement("div");
     div.class = "post-container";
+
+    const userMock = {name: "Lucas", lastName: "Nunes"}
+
+    const divPostHeaderContainer = createDivPostHeaderContainer(userMock);
+    const divPostDate = createDivPostDate(userMock);
+    const divPostContent = createDivPostContent(userMock);
+    const divPostImage = createDivPostImage(userMock);
+    const divPostButton = createDivPostButton(userMock);
+
+    div.appendChild(divPostHeaderContainer);
+    div.appendChild(divPostDate);
+    div.appendChild(divPostContent);
+    div.appendChild(divPostImage);
+    div.appendChild(divPostButton);
+
+    return div;
 }
 
 function createDivPostHeaderContainer(user) {
     const div = document.createElement("div");
     div.class = "post-header-container";
-    
+
     const img = document.createElement("img");
     img.alt = "Foto de Perfil";
     img.class = "post-user-profile-photo";
     img.src = "../../assets/fotoPerfil.jpg";//buildPathProfilePhoto() + user.photoFileName;
-    
-    const fullName = user.name;
+
+    let fullName = user.name;
     if (user.lastName != null) {
         fullName = fullName + " " + user.lastName;
     }
 
     const span = document.createElement("span");
     span.class = "user-name-post";
-    span.value = "UsuarioFulano";//fullName
+    span.value = fullName;//fullName
 
     div.appendChild(img);
     div.appendChild(span);
